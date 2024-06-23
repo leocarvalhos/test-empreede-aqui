b7de0c64806428bc38ef587524ad0baafbf3ff9b
Leandro <dev.leandrocarvalho@gmail.com>
2024-06-22 22:08:36 -0300

    add repositories no service

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Account } from './entities/account.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AccountsService {
+  constructor(
+    @InjectRepository(Account)
+    private usersRepository: Repository<Account>,
+  ) {}
+
   create(createAccountDto: CreateAccountDto) {
     return 'This action adds a new account';
   }
