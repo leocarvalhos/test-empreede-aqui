c40cb2bf18304a0f413908a97b25a9f0ea0eedfd
Leandro <dev.leandrocarvalho@gmail.com>
2024-06-25 15:42:00 -0300

    feat: auth em account

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -7,40 +7,48 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('accounts')
 export class AccountsController {
   constructor(private readonly accountsService: AccountsService) {}
 
+  @UseGuards(AuthGuard)
   @Post('withdrawal/:id')
   withdrawal(@Param('id') id: string, @Body() amount: number) {
     return this.accountsService.withdrawal(id, amount);
   }
 
+  @UseGuards(AuthGuard)
   @Post('deposit/:id')
   deposit(@Param('id') id: string, @Body() amount: number) {
     return this.accountsService.deposit(id, amount);
   }
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createAccountDto: CreateAccountDto) {
     return this.accountsService.create(createAccountDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOneById(@Param('id') id: string) {
     return this.accountsService.findOneById(id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAccountDto: UpdateAccountDto) {
     return this.accountsService.update(id, updateAccountDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.accountsService.remove(id);
