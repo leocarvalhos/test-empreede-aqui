906eefdf770d54cc7ab2327928dfcf1c3c47f1cb
Leandro <dev.leandrocarvalho@gmail.com>
2024-06-25 02:40:04 -0300

    reafactor: update dto

--- src/accounts/dto/update-account.dto.ts
+++ src/accounts/dto/update-account.dto.ts
@@ -1,8 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAccountDto } from './create-account.dto';
-import { IsDecimal, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class UpdateAccountDto extends PartialType(CreateAccountDto) {
   @IsString()
-  name?: string;
+  name: string;
 }
